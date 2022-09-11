22c1fc68998bf22eb54a6cf489c2e4645c9e66fe
Oliver Fox-Smith <ofoxsmith@outlook.com>
2022-09-12 08:28:53 +1000

    add properties and functions to base gui element

--- GuiElement.lua
+++ GuiElement.lua
@@ -1,6 +1,24 @@
 -- Gui element parent class
 -- inherited by all elements 
 local GuiElement = {};
+GuiElement._z = 0;
+GuiElement.style = {}
+GuiElement.children = {}
+GuiElement.parent = {}
+function GuiElement:AddChild(child)
+    child.parent = self
+    table.insert(self.children, child)
+end
+
+function GuiElement:GetZ()
+    local at = self
+    local z = 0
+    while true do
+        z = z - 1000
+        if at.parent == nil then return z end
+        at = at.parent
+    end 
+end 
 function GuiElement:New()
     local o = {}
     setmetatable(o, self)
