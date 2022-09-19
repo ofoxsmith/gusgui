17d74e44ba5bcbd3aac5602d591716760dca2434
Oliver Fox-Smith <ofoxsmith@outlook.com>
2022-09-19 13:58:18 +1000

    implement GetBaseElementSize function in HLayout

--- HLayout.lua
+++ HLayout.lua
@@ -8,7 +8,16 @@ local HLayout = class(GuiElement, function(o, id, align, config)
 end)
 
 function HLayout:GetBaseElementSize()
-    
+    local totalW = 0
+    local totalH = 0
+    for i = 1, #self.children do local child = self.children[i]
+        local size = child:GetElementSize()
+        size.width = math.max(size.width + child.config.margin.left + child.config.margin.right, child.config.overrideWidth or 0)
+        size.height = math.max(size.height + child.config.margin.top + child.config.margin.bottom, child.config.overrideHeight or 0)
+        totalW = totalW + size.width
+        totalH = totalH > size.height and totalH or size.height 
+    end
+    return totalW, totalH
 end
 
 function HLayout:GetManagedXY()
