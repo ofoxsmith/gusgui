f1356b003e71918deffa69c9f9e959693e800c0a
Oliver Fox-Smith <ofoxsmith@outlook.com>
2022-09-24 16:16:23 +1000

    update validate.lua to support state

--- validate.lua
+++ validate.lua
@@ -1,9 +1,17 @@
 local function validate(applyTo, id, s, elem, o)
     if o then
         local typeof = type(elem)
+        if typeof == "table" and elem["_type"] and elem["_value"] then
+            applyTo[s.name] = elem
+            return true
+        end
         if (typeof == "nil" and s.default == nil) or (typeof ~= s.type and typeof ~= "nil" and type(s.type) ~= "table") then
-            if type(s.type == "table") then error("GUI: Invalid value for " .. s.name .. " on element \"" .. id .."\"", 2)            end
-            error("GUI: Invalid value for " .. s.name .. " on element \"" .. id .."\" (" .. s.type .. " expected, got " .. typeof .. ")", 2)
+            if type(s.type == "table") then
+                error("GUI: Invalid value for " .. s.name .. " on element \"" .. id .. "\"", 2)
+            end
+            error(
+                "GUI: Invalid value for " .. s.name .. " on element \"" .. id .. "\" (" .. s.type .. " expected, got " ..
+                    typeof .. ")", 2)
         end
         if (typeof == "nil") then
             elem = s.default
@@ -11,7 +19,7 @@ local function validate(applyTo, id, s, elem, o)
         if typeof == "table" then
             for k, v in pairs(s.type) do
                 if (type(elem[k]) ~= v) then
-                    error("GUI: Invalid table value for " .. s.name .. " on element \"" .. id .."\"", 2)
+                    error("GUI: Invalid table value for " .. s.name .. " on element \"" .. id .. "\"", 2)
                 end
             end
         end
@@ -21,21 +29,29 @@ local function validate(applyTo, id, s, elem, o)
     for _, v in ipairs(s) do
         local value = elem[v.name]
         local typeof = type(value)
-        if (typeof == "nil" and v.default == nil) or (typeof ~= v.type and typeof ~= "nil" and type(v.type) ~= "table") then
-            if type(v.type == "table") then error("GUI: Invalid value for " .. v.name .. " on element \"" .. id .."\"", 2)            end
-            error("GUI: Invalid value for " .. v.name .. " on element \"" .. id .."\" (" .. v.type .. " expected, got " .. typeof .. ")", 2)
-        end
-        if (typeof == "nil") then
-            value = v.default
-        end
-        if typeof == "table" then
-            for k, t in pairs(v.type) do
-                if (type(value[k]) ~= t) then
-                    error("GUI: Invalid table value for " .. v.name .. " on element \"" .. id .."\"", 2)
+        if typeof == "table" and value["_type"] and value["_value"] then
+            applyTo[v.name] = value
+        else
+            if (typeof == "nil" and v.default == nil) or
+                (typeof ~= v.type and typeof ~= "nil" and type(v.type) ~= "table") then
+                if type(v.type == "table") then
+                    error("GUI: Invalid value for " .. v.name .. " on element \"" .. id .. "\"", 2)
+                end
+                error("GUI: Invalid value for " .. v.name .. " on element \"" .. id .. "\" (" .. v.type ..
+                          " expected, got " .. typeof .. ")", 2)
+            end
+            if (typeof == "nil") then
+                value = v.default
+            end
+            if typeof == "table" then
+                for k, t in pairs(v.type) do
+                    if (type(value[k]) ~= t) then
+                        error("GUI: Invalid table value for " .. v.name .. " on element \"" .. id .. "\"", 2)
+                    end
                 end
             end
+            applyTo[v.name] = value
         end
-        applyTo[v.name] = value
     end
     return true
 end
