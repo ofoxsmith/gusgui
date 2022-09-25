bcf175c85265af9fa9ccaedcd72f6a5fba2a7f2e
Oliver Fox-Smith <ofoxsmith@outlook.com>
2022-09-25 15:24:13 +1000

    update input validation

--- GuiElement.lua
+++ GuiElement.lua
@@ -4,11 +4,20 @@ dofile_once("GUSGUI_PATHclass.lua")
 -- Elements that manage other child elements implement a GetManagedXY function, which allows children to get x, y relative to parent position and config
 -- and a Draw method, which draws the element using the Gui API
 local GuiElement = class(function(Element, config)
-    if config.id == nil then error("GUI: Invalid construction of element (id is required)") end
+    if config.id == nil then
+        error("GUI: Invalid construction of element (id is required)")
+    end
     Element.id = config.id
     config.id = nil;
     Element.config = {}
-    Element._rawconfig = config
+    Element._rawconfig = {}
+    for k = 1, #self.baseValidator do
+        local v = self.baseValidator[k]
+        local valid, nv, err = v.validate(config[v.name])
+        if valid and (nv ~= nil) then Element._rawconfig[v.name] = nv 
+        elseif valid then Element._rawconfig[v.name] = config[v.name]
+        elseif err then error(err:format(Element.id), 4) end
+    end
     Element.gui = nil
     Element.allowChildren = false
     setmetatable(Element.config, {
@@ -16,33 +25,49 @@ local GuiElement = class(function(Element, config)
             return Element._rawconfig[k]
         end,
         __newindex = function(t, k, v)
-            Element._rawconfig[k] = v
+            local valid, nv, err = self.baseValidator[k].validate(v)
+            if valid and (nv ~= nil) then
+                Element._rawconfig[v.name] = nv
+            elseif valid then
+                Element._rawconfig[v.name] = v
+            elseif err then
+                error(err:format(Element.id), 2)
+            end
         end
     })
     Element.children = {}
     Element.rootNode = false
 end)
 
-
-function GuiElement:ResolveValue(a, t) 
-    if type(a) ~= "table" then return a end
-    if a._type == "state" and type(a.value) == "string" then return self.gui.GetState(a) end
-    if a._type == "global" and type(a.value) == "string" then return GlobalsGetValue(a.value) end
+function GuiElement:ResolveValue(a, t)
+    if type(a) ~= "table" then
+        return a
+    end
+    if a._type == "state" and type(a.value) == "string" then
+        return self.gui.GetState(a)
+    end
+    if a._type == "global" and type(a.value) == "string" then
+        return GlobalsGetValue(a.value)
+    end
     return a
-end 
+end
 
-function GuiElement:GetDepthInTree() 
+function GuiElement:GetDepthInTree()
     local at = self
     local d = 0
     while true do
         d = d + 1
-        if (at.parent == nil) then return d end
+        if (at.parent == nil) then
+            return d
+        end
         at = at.parent
     end
 end
 
 function GuiElement:AddChild(child)
-    if not self.allowChildren then error("GUI: " .. self.type .. " cannot have child element") end
+    if not self.allowChildren then
+        error("GUI: " .. self.type .. " cannot have child element")
+    end
     local function testID(i)
         for k = 1, #self.gui.ids do
             if (self.gui.ids[k] == i) then
@@ -73,7 +98,9 @@ function GuiElement:RemoveChild(childName)
             table.remove(self.children, i)
             local newids = {}
             for i, a in ipairs(self.gui.ids) do
-                if a ~= v.id then table.insert(newids, a) end
+                if a ~= v.id then
+                    table.insert(newids, a)
+                end
             end
             self.gui.ids = newids
             break
@@ -100,20 +127,24 @@ function GuiElement:GetElementSize()
 end
 
 function GuiElement:RenderBorder(x, y, w, h)
-      local width = math.max((self.config.overrideWidth or 0) - 2, w + self.config.padding.left + self.config.padding.right)
-      local height = math.max((self.config.overrideHeight or 0) - 2, h + self.config.padding.top + self.config.padding.bottom)
-      GuiZSetForNextWidget(self.gui.guiobj, self.z + 1)
-      GuiImageNinePiece(self.gui.guiobj, self.gui.nextID(), x + 1, y + 1, width, height, 1, "GUSGUI_PATHborder.png")
+    local width = math.max((self.config.overrideWidth or 0) - 2,
+        w + self.config.padding.left + self.config.padding.right)
+    local height = math.max((self.config.overrideHeight or 0) - 2,
+        h + self.config.padding.top + self.config.padding.bottom)
+    GuiZSetForNextWidget(self.gui.guiobj, self.z + 1)
+    GuiImageNinePiece(self.gui.guiobj, self.gui.nextID(), x + 1, y + 1, width, height, 1, "GUSGUI_PATHborder.png")
 end
 
 function GuiElement:RenderBackground(x, y, w, h)
     local border = (self:ResolveValue(self.config.drawBorder) and 2 or 0)
-    local width = math.max((self.config.overrideWidth or 0) - border, w + self.config.padding.left + self.config.padding.right) - 1
-    local height = math.max((self.config.overrideHeight or 0) - border, h + self.config.padding.top + self.config.padding.bottom) - 1
+    local width = math.max((self.config.overrideWidth or 0) - border,
+        w + self.config.padding.left + self.config.padding.right) - 1
+    local height = math.max((self.config.overrideHeight or 0) - border,
+        h + self.config.padding.top + self.config.padding.bottom) - 1
     GuiZSetForNextWidget(self.gui.guiobj, self.z + 3)
-    GuiImageNinePiece(self.gui.guiobj, self.gui.nextID(), x + (border/2), y + (border/2), width, height, 1, "GUSGUI_PATHbg.png")
+    GuiImageNinePiece(self.gui.guiobj, self.gui.nextID(), x + (border / 2), y + (border / 2), width, height, 1,
+        "GUSGUI_PATHbg.png")
 end
-
 
 function GuiElement:Remove()
     for i, v in ipairs(self.parent.children) do
@@ -123,5 +154,194 @@ function GuiElement:Remove()
         end
     end
 end
+
+Gui.baseValidator = {{
+    name = "drawBorder",
+    validate = function(o)
+        local t = type(o)
+        if o == nil then
+            return true, false, nil
+        end
+        if t == "table" and t["_type"] ~= nil and t["value"] then
+            return true, nil, nil
+        end
+        if t == "boolean" then
+            return true, nil, nil
+        end
+        return false, nil, "GUI: Invalid value for drawBorder on element \"%s\""
+    end
+}, {
+    name = "drawBackground",
+    validate = function(o)
+        local t = type(o)
+        if o == nil then
+            return true, false, nil
+        end
+        if t == "table" and t["_type"] ~= nil and t["value"] then
+            return true, nil, nil
+        end
+        if t == "boolean" then
+            return true, nil, nil
+        end
+        return false, nil, "GUI: Invalid value for drawBackground on element \"%s\""
+    end
+}, {
+    name = "overrideWidth",
+    validate = function(o)
+        local t = type(o)
+        if o == nil then
+            return true, 0, nil
+        end
+        if t == "table" and t["_type"] ~= nil and t["value"] then
+            return true, nil, nil
+        end
+        if t == "number" then
+            if o <= 0 then
+                return false, nil, "GUI: Invalid value for overrideWidth on element \"%s\" (must be greater than 0)"
+            end
+            return true, nil, nil
+        end
+        return false, nil, "GUI: Invalid value for overrideWidth on element \"%s\""
+    end
+}, {
+    name = "overrideHeight",
+    validate = function(o)
+        local t = type(o)
+        if o == nil then
+            return true, 0, nil
+        end
+        if t == "table" and t["_type"] ~= nil and t["value"] then
+            return true, nil, nil
+        end
+        if t == "number" then
+            if o <= 0 then
+                return false, nil, "GUI: Invalid value for overrideHeight on element \"%s\" (must be greater than 0)"
+            end
+            return true, nil, nil
+        end
+        return false, nil, "GUI: Invalid value for overrideHeight on element \"%s\""
+    end
+}, {
+    name = "verticalAlign",
+    validate = function(o)
+        local t = type(o)
+        if o == nil then
+            return true, 0, nil
+        end
+        if t == "table" and t["_type"] ~= nil and t["value"] then
+            return true, nil, nil
+        end
+        if t == "number" then
+            if 0 < o and o < 1 then
+                return false, nil,
+                    "GUI: Invalid value for verticalAlign on element \"%s\" (value did not match 0 ≤ value ≤ 1)"
+            end
+            return true, nil, nil
+        end
+        return false, nil, "GUI: Invalid value for verticalAlign on element \"%s\""
+    end
+}, {
+    name = "horizontalAlign",
+    type = "number",
+    validate = function(o)
+        local t = type(o)
+        if o == nil then
+            return true, 0, nil
+        end
+        if t == "table" and t["_type"] ~= nil and t["value"] then
+            return true, nil, nil
+        end
+        if t == "number" then
+            if 0 < o and o < 1 then
+                return false, nil,
+                    "GUI: Invalid value for horizontalAlign on element \"%s\" (value did not match 0 ≤ value ≤ 1)"
+            end
+            return true, nil, nil
+        end
+        return false, nil, "GUI: Invalid value for horizontalAlign on element \"%s\""
+    end
+}, {
+    name = "margin",
+    validate = function(o)
+        local t = type(o)
+        if o == nil then
+            return true, {
+                top = 0,
+                bottom = 0,
+                left = 0,
+                right = 0
+            }, nil
+        end
+        if t == "number" then
+            return true, {
+                top = o,
+                bottom = o,
+                left = o,
+                right = o
+            }, nil
+        end
+        if t == "table" then
+            local m = {
+                top = o["top"],
+                bottom = o["bottom"],
+                left = o["left"],
+                right = o["right"]
+            }
+            for _, v in ipairs({"top", "bottom", "left", "right"}) do
+                local ty = type(m[v])
+                if ty == "nil" then
+                    m[v] = 0
+                elseif ty == "number" then
+                elseif ty == "table" and m[v] ~= nil and m[v] ~= nil then
+                else
+                    return false, nil, "GUI: Invalid value for margin " .. v .. " on element \"%s\""
+                end
+            end
+            return true, m, nil;
+        end
+        return false, nil, "GUI: Invalid value for margin on element \"%s\""
+    end
+}, {
+    name = "padding",
+    validate = function(o)
+        local t = type(o)
+        if o == nil then
+            return true, {
+                top = 0,
+                bottom = 0,
+                left = 0,
+                right = 0
+            }, nil
+        end
+        if t == "number" then
+            return true, {
+                top = o,
+                bottom = o,
+                left = o,
+                right = o
+            }, nil
+        end
+        if t == "table" then
+            local m = {
+                top = o["top"],
+                bottom = o["bottom"],
+                left = o["left"],
+                right = o["right"]
+            }
+            for _, v in ipairs({"top", "bottom", "left", "right"}) do
+                local ty = type(m[v])
+                if ty == "nil" then
+                    m[v] = 0
+                elseif ty == "number" then
+                elseif ty == "table" and m[v] ~= nil and m[v] ~= nil then
+                else
+                    return false, nil, "GUI: Invalid value for padding " .. v .. " on element \"%s\""
+                end
+            end
+            return true, m, nil;
+        end
+        return false, nil, "GUI: Invalid value for padding on element \"%s\""
+    end
+}}
 
 return GuiElement
